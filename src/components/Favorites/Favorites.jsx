ab2f9a24f168aae6fa12b2f100bc44a94c00e607
Kshamenk Benitez <kbelpirata007@gmail.com>
2023-03-13 17:54:20 -0300

    175413032023

--- src/components/Favorites/Favorites.jsx
+++ src/components/Favorites/Favorites.jsx
@@ -1,30 +1,77 @@
-import React from 'react';
-import { connect }  from 'react-redux';
+import React, { useState } from "react";
+import { connect } from "react-redux";
+import { filterCards, orderCards } from "../../redux/actions.js";
+import { useDispatch } from "react-redux";
+import styles from '../Favorites/Favorites.module.css';
 
+function Favorites({ myFavorites, allCharacters }) {
+  const dispatch = useDispatch();
 
- function Favorites({ myFavorites }) {
+  const [order, setOrder] = useState("Ascendente");
+  const [gender, setGender] = useState("all");
 
-    return (
-        <div>
-            <h1>Favorites</h1>
-            {
-                myFavorites.map((fav) => {
-                    return (
-                        <div>
-                            <h2>{fav.name}</h2>
-                            <img src={fav.image} alt="" />
-                        </div>
-                    )
-                })
-            }
-        </div>
-    )
+  const handleOrderChange = (e) => {
+    dispatch(orderCards(e.target.value));
+    setOrder(e.target.value);
+  };
+
+  const handleGenderChange = (e) => {
+    dispatch(filterCards(e.target.value));
+    setGender(e.target.value);
+  };
+
+  const filteredFavorites = myFavorites.filter(
+    (fav) => gender === "all" || fav.gender === gender
+  );
+
+  const sortedFavorites = [...filteredFavorites].sort((a, b) => {
+    if (order === "Ascendente") {
+      return a.id - b.id;
+    } else if (order === "Descendente") {
+      return b.id - a.id;
+    }
+    return 0;
+  });
+
+  return (
+    <div>
+      <h1 className={styles.title}>Favorites</h1>
+      <div className={styles.selectContainer}>
+        <label className={styles.label}>Ordenar por:</label>
+        <select className={styles.select} value={order} onChange={handleOrderChange}>
+          <option value="Ascendente">Ascendente</option>
+          <option value="Descendente">Descendente</option>
+        </select>
+      </div>
+      <div className={styles.selectContainer}>
+        <label className={styles.label}>Filtrar por género:</label>
+        <select className={styles.select} value={gender} onChange={handleGenderChange}>
+          <option value="all">Todos</option>
+          <option value="Male">Masculino</option>
+          <option value="Female">Femenino</option>
+          <option value="Genderless">Sin género</option>
+          <option value="unknown">Desconocido</option>
+        </select>
+      </div>
+      <div className={styles.container} >
+      {sortedFavorites.map((fav) => {
+        return (
+          <div key={fav.id} className={styles.item}>
+            <h2 className={styles.name}>{fav.name}</h2>
+            <img src={fav.image} alt={fav.name} className={styles.image} />
+          </div>
+        );
+      })}
+      </div>
+    </div>
+  );
 }
 
 const mapStateToProps = (state) => {
-    return {
-        myFavorites: state.myFavorites
-    }
-}
+  return {
+    myFavorites: state.myFavorites,
+    allCharacters: state.allCharacters,
+  };
+};
 
-export default connect( mapStateToProps, null ) (Favorites)
+export default connect(mapStateToProps, null)(Favorites);
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,31 +1,48 @@
-import { ADD_CHARACTER, DELETE_CHARACTER } from "./actions"
+import { ADD_CHARACTER, DELETE_CHARACTER, FILTER_CARDS, ORDER_CARDS } from "./actions"
 
 const initialState = {
-    myFavorites: [],
-}
-
+  myFavorites: [],
+  allCharacters: [],
+};
 
 function reducer(state = initialState, action) {
-    switch (action.type) {
-        case ADD_CHARACTER:
-        return {
-            ...state,
-            myFavorites: [...state.myFavorites, action.payload ]
+  switch (action.type) {
+    case ADD_CHARACTER:
+      return {
+        ...state,
+        myFavorites: [...state.myFavorites, action.payload ],
+      };
+    case DELETE_CHARACTER:
+      return {
+        ...state,
+        myFavorites: state.myFavorites.filter((char) => {
+          return char.id !== action.payload;
+        }),
+      };
+    case FILTER_CARDS:     // en una variable guardp el valor del resultado q me da filtrar el state de todos los char
+      const filteredCharacters = state.allCharacters.filter((char) => {
+        return char.gender === action.payload;    //compara el genero c el valor del payload que recibio como accion
+      });
+      return {
+        ...state,
+        myFavorites: filteredCharacters,
+      };
+    case ORDER_CARDS:
+      const sortedCharacters = [...state.allCharacters].sort((a, b) => { //usamos el metodo sort, hacemos una comparacion de a y b
+        if (action.payload === "Ascendente") { 
+          return a.id - b.id; // si es ascendente q retorne el id de a menos el id de b
+        } else if (action.payload === "Descendente") {   //la función devuelve la diferencia de los IDs en orden inverso (b.id - a.id)
+          return b.id - a.id;
         }
-        case DELETE_CHARACTER:
-            return {
-                ...state,
-                myFavorites: state.myFavorites.filter((char) => {
-                    return char.id !== action.payload
-                })
-
-            }
-        default:
-            return {...state}
-    }
-
-
-    
+        return 0;
+      });
+      return {
+        ...state,
+        myFavorites: sortedCharacters,
+      };
+    default:
+      return {...state};
+  }
 }
 
-export default reducer
+export default reducer;
